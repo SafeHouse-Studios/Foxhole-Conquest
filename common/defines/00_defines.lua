cb70cb0a97751a2583b1e126ddb7258a903cb821
KBear44 <66195150+KBear44@users.noreply.github.com>
2021-06-02 19:27:49 -0700

    Changed Clan Start Date and Changed Game End Date

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -2,7 +2,7 @@ NDefines = {
 
 NGame = {
 	START_DATE = "0782.1.1.12",
-	END_DATE = "0802.1.1.12",
+	END_DATE = "0800.1.1.12",
 	MAP_SCALE_PIXEL_TO_KM = 7.114,					-- Yes, we did the math
 	SAVE_VERSION = 7,								-- 1.8.0
 	CHECKSUM_SALT = "zwOdv5d9wm9uDSOT",				-- Data to modify generated checksum when game binaries have changed but not any content files.
